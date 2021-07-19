ef2fca67de38c55d2714adcc63aa70f34921c5bb
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2021-07-20 03:12:59 +0600

    добавляет иконки на кнопки

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,13 +3,16 @@
     openMenuBtn: document.querySelector('.menu-open'),
     closeMenuBtn: document.querySelector('.menu-close'),
     menu: document.querySelector('.mobile-menu'),
+    // hero: document.querySelector('.hero'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
+    // document.body.classList.toggle('backdrop');
     document.body.classList.toggle('menu-open');
     refs.menu.classList.toggle('is--hidden');
+    // refs.hero.classList.toggle('backdrop');
   }
 })();
