806cab11fd7a5eac6e1dfb739e60febfff604cd5
SergeyBelokopytnyy <82495436+SergeyBelokopytnyy@users.noreply.github.com>
2021-07-22 18:55:10 +0600

    Восстанавливает работоспособность модальных окон (#88)

--- src/js/modal-about.js
+++ src/js/modal-about.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-open-about]'),
+    closeModalBtn: document.querySelector('[data-modal-close-about]'),
+    modal: document.querySelector('[data-modal-about]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
